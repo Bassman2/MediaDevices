a0e6b218203cd6a815353a2dae335784df09ff2d
Ralf Beckers <ralf.beckers@elektrobit.com>
2018-01-08 16:54:19 +0100

    Add MediaDriveInfo

--- MediaDevices/MediaDevice.cs
+++ MediaDevices/MediaDevice.cs
@@ -1177,6 +1177,11 @@ namespace MediaDevices
             return new MediaDirectoryInfo(this, item);
         }
 
+        public MediaDriveInfo[] GetDrives()
+        {
+            return this.FunctionalObjects(FunctionalCategory.Storage)?.Select(o => new MediaDriveInfo(this, o)).ToArray();
+        }
+
         /// <summary>
         /// Gets a new instance of the root MediaDirectoryInfo class, which acts as a wrapper for the root directory path.
         /// </summary>
