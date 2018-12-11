d19d504c90b6ab69f9a2c475ab1bdeb7cf2f568a
Ralf Beckers <ralf.beckers@elektrobit.com>
2018-12-11 11:08:41 +0100

    Original interops for any, x86 and x64

---
+++ MediaDevices/InteropOriginal/Decompile.cmd
@@ -0,0 +1,9 @@
+ildasm Interop.PortableDeviceApiLib.any.dll /out:Interop.PortableDeviceApiLib.any.il
+ildasm Interop.PortableDeviceApiLib.x64.dll /out:Interop.PortableDeviceApiLib.x64.il
+ildasm Interop.PortableDeviceApiLib.x86.dll /out:Interop.PortableDeviceApiLib.x86.il
+ildasm Interop.PortableDeviceTypesLib.any.dll /out:Interop.PortableDeviceTypesLib.any.il
+ildasm Interop.PortableDeviceTypesLib.x64.dll /out:Interop.PortableDeviceTypesLib.x64.il
+ildasm Interop.PortableDeviceTypesLib.x86.dll /out:Interop.PortableDeviceTypesLib.x86.il
+
+
+
