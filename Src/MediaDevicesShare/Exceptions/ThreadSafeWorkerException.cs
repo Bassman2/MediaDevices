7371e1a92c513065e38879e61a445afc12821f61
Ralf Beckers <ralf.beckers@hotmail.com>
2026-07-30 21:18:59 +0200

    Update

---
+++ Src/MediaDevicesShare/Exceptions/ThreadSafeWorkerException.cs
@@ -0,0 +1,35 @@
+﻿namespace MediaDevices;
+
+public class ThreadSafeWorkerException(string? message) : Exception(message)
+{
+    internal static void ThrowIfNotInside(
+        [CallerLineNumber] int lineNumber = 0,
+        [CallerFilePath] string filePath = "",
+        [CallerMemberName] string memberName = "")
+    {
+        if (Environment.CurrentManagedThreadId != ThreadSafeWorker.ThreadId)
+        {
+            string caller = Caller(lineNumber, filePath, memberName);
+            throw new ThreadSafeWorkerException($"Wrong Thread! {caller}");
+        }
+        if (Thread.CurrentThread.GetApartmentState() != ApartmentState.MTA)
+        {
+            string caller = Caller(lineNumber, filePath, memberName);
+            throw new ThreadSafeWorkerException($"Wrong Thread ApartmentState! {caller}");
+        }
+    }
+
+    internal static void ThrowIfNotOutside(
+        [CallerLineNumber] int lineNumber = 0,
+        [CallerFilePath] string filePath = "",
+        [CallerMemberName] string memberName = "")
+    {
+        if (Environment.CurrentManagedThreadId == ThreadSafeWorker.ThreadId)
+        {
+            string caller = Caller(lineNumber, filePath, memberName);
+            throw new ThreadSafeWorkerException($"Already in Wrong Thread. Deadlock will occure! {caller}");
+        }
+    }
+
+    private static string Caller(int lineNumber, string filePath, string memberName) => $"{filePath} ({lineNumber}) {memberName}";
+}
